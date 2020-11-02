5ee781f353d02032de166dddc4ef50ecf90e89f6
Burenko <burenkoanton@gmail.com>
2020-11-02 15:25:05 +0100

    add scratch of a TagContainer

---
+++ src/src/components/TagContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="btn-group-toggle" data-toggle="buttons">
+    <li v-for="tagName in tagNames" v-bind:key="tagName">
+      <label class="btn btn-secondary active">
+        <input type="checkbox" checked /> {{ tagName }}
+      </label>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    height: String,
+    width: String,
+    tagNames: Array,
+    clickable: Boolean,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
