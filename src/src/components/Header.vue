53b8290075795d9fc435d440b11d58591dbec2a3
Ledditz <drlucas97@gmail.com>
2020-11-09 14:25:04 +0100

    add dropdown

--- src/src/components/Header.vue
+++ src/src/components/Header.vue
@@ -1,10 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a v-if="bools.hasReturnBtn">
-      <i class="material-icons icons" @click="this.$router.go(-1)">
+    <div class="btn-back">
+      <i
+        class="material-icons icons"
+        v-if="bools.hasReturnBtn"
+        @click="this.$router.go(-1)"
+      >
         arrow_back</i
       >
-    </a>
+    </div>
     <div class="navbar-brand title">{{ pageName }}</div>
     <button
       class="navbar-toggler"
@@ -26,16 +30,28 @@
         <router-link v-if="bools.hasSearchBtn" to="/">
           <i class="material-icons icons">search</i>
         </router-link>
-        <router-link to="/profile/">
-          <i class="material-icons icons">account_circle</i>
-        </router-link>
+        <div class="dropdown">
+          <button class="dropbtn bg-light">
+            <i class="material-icons icons">account_circle</i>
+          </button>
+          <div class="dropdown-content">
+            <router-link
+              to="{name:'Profile', params: {userId:userId}}"
+              v-if="isLoggedIn"
+            >
+              Mein Profil
+            </router-link>
+            <a href="#" v-if="isLoggedIn" @click="doLogout">Logout</a>
+            <a href="#" v-if="!isLoggedIn" @click="doLogin">Login</a>
+          </div>
+        </div>
       </form>
     </div>
   </nav>
 </template>
 
 <script>
-import { onMounted, ref, reactive } from "vue";
+import { reactive, ref } from "vue";
 
 export default {
   name: "Header",
@@ -49,6 +65,8 @@ export default {
       hasSearchBtn: true,
       hasHomeBtn: true,
     });
+    const isLoggedIn = ref(false);
+    // check if user is Logged in
 
     switch (props.pageName) {
       case "Willkommen":
@@ -66,11 +84,18 @@ export default {
         break;
     }
 
-    return { bools };
+    return { bools, isLoggedIn };
   },
 
   methods: {
-    getType() {},
+    doLogout() {
+      // logout the User
+      console.log("Logout not implemented.");
+    },
+    doLogin() {
+      // login the User
+      console.log("Login not implemented.");
+    },
   },
 };
 </script>
@@ -85,5 +110,50 @@ export default {
 .title {
   font-size: 2.5rem;
   font-weight: bold;
+}
+
+.btn-back {
+  margin-bottom: -0.5rem;
+}
+
+.btn-back:hover {
+  cursor: pointer;
+}
+
+.dropbtn {
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  left: -4.5rem;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
 }
 </style>
