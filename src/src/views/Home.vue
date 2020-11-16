8cadc3f6b7db295b43d636e0a88f27e1b3cd281c
Ledditz <drlucas97@gmail.com>
2020-11-16 14:50:44 +0100

    start working on CardContainer

--- src/src/views/Home.vue
+++ src/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <Header pageName="Willkommen" userId="7357" />
+    <Header pageName="Willkommen" :userId="7357" />
+    <CardContainer title="Mein Garten" :plantIds="plantIds" isVertical />
     <TagContainer v-bind:tagNames="tagNames" :clickable="true" />
     checked:
     <p @get-choosen-tags="showTags"></p>
@@ -11,16 +12,19 @@
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
 import TagContainer from "@/components/TagContainer.vue";
+import CardContainer from "@/components/CardContainer.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
     TagContainer,
+    CardContainer,
   },
   data() {
     return {
       tagNames: ["a", "aa", "aab", "aabb", "aabbc", "aabbcc", "abbbaaac"],
+      plantIds: [7357, 545, 46, 5, 7357, 545, 46, 5, 7357, 545, 46],
     };
   },
   methods: {
