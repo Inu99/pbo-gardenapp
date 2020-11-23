356f10ff48834e217529ec038ed07fe5a2dc9924
Burenko <burenkoanton@gmail.com>
2020-11-23 12:49:58 +0100

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+const settings = { timestampsInSnapshots: true };
+
+const config = {
+    apiKey: "AIzaSyAvNKFv1Lwm9_rADLXL4ECapsbW4xMxguI",
+    authDomain: "pbo-gartenapp.firebaseapp.com",
+    databaseURL: "https://pbo-gartenapp.firebaseio.com/",
+    projectId: "pbo-gartenapp",
+    storageBucket: "pbo-gartenapp.appspot.com",
+    messagingSenderId: "105407469441",
+    appId: "1:105407469441:web:7ee2fbd366b32fde7fba80"
+};
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
