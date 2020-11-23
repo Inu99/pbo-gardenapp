01068a537b130d2f2a50574fa9ab042d2e3aedd4
Inu99 <vivalover@quantentunnel.de>
2020-11-23 15:15:35 +0100

    feature added header of plantview

--- src/src/views/PflanzenView.vue
+++ src/src/views/PflanzenView.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="PflanzenView">
-    <h2>PFLANZENVIEW:</h2>
+    <Header
+      :pageName="this.data.plant[$route.params.plantId].name"
+      :userId="7357"
+    />
     Pflanze mit id: {{ $route.params.plantId }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Header from "@/components/Header.vue";
 
 export default {
   name: "PflanzenView",
-  components: {},
+  components: {
+    Header,
+  },
+  data() {
+    this.data = require("../data/plants.json");
+  },
 };
 </script>
